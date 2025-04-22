013701fb21c333357060acc18488b095b2842aea
Luan <83055293+Luanzinhob5@users.noreply.github.com>
2025-04-22 16:32:19 -0300

    day 16

--- day16/coffe_machine_oop/main.py
+++ day16/coffe_machine_oop/main.py
@@ -5,13 +5,22 @@ from coffee_maker import CoffeeMaker
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
+ 
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    guess = input(f"What would you like? ({options}): ")
 
 
-guess = input(f"What would you like? ({menu.get_items()}): ")
+    if guess == "off":
+        is_on = False
+    elif guess == "report":
+        coffee_maker.report
+        money_machine.report
+    else:
+        drink = menu.find_drink(guess)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
-if guess =="report":
-    coffee_maker.report()
-    money_machine.report()
-else:
-    menu.find_drink(guess)
-    
+
